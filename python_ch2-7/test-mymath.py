2a7eb0beb415284f0a71a459ac35672eafaaa9d1
ydhwa <ydhwa_18@naver.com>
2019-06-17 11:25:10 +0900

    module

---
+++ python_ch2-7/test-mymath.py
@@ -0,0 +1,10 @@
+import sys
+import mymath
+
+
+# sys.path.append('/cafe24/PycharmProjects/python-modules')
+
+print(mymath.pi)
+print(mymath.add(10, 20))
+print(mymath.area_circle(10))
+
---
+++ python_ch2-7/test-mymod.py
@@ -0,0 +1,6 @@
+import mymod
+
+print(mymod.add(10, 20))
+print(mymod.subtract(10, 20))
+print(mymod.multiply(10, 20))
+print(mymod.devide(10, 20))
