f99ece4631c38a3a9e83a22f922e07b771174d94
ydhwa <ydhwa_18@naver.com>
2019-06-19 10:21:02 +0900

    lambda 연습

---
+++ lambda/map_filter.py
@@ -0,0 +1,23 @@
+def f(x):
+    return x ** 2
+
+
+# map(f, [1, 2, 3, 4])
+
+# map은 iterator 객체이다.
+it = map(lambda x: print(x**2, sep=' '), [1, 2, 3, 4])
+next(it)
+next(it)
+next(it)
+next(it)
+print('===========================')
+
+
+# 만듦과 동시에 출력시키려면?
+# list에 iterator를 인자로 주면 list가 알아서 next를 시켜(next를 시킨다면 그 과정에서 실행이 됨) 리스트에 넣는다.
+lst = list(map(lambda x: x**2, [1, 2, 3, 4]))
+print(lst)
+print('===========================')
+
+
+list(map(lambda x: print(x**2, sep=' '), [1, 2, 3, 4]))
